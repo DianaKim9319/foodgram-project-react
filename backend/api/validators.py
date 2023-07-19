fc72bc7014572454e925fb05a8fd2e1fdaf9be20
Diana Kim <Dianakim.1993@yandex.ru>
2023-07-19 17:14:02 +0900

    Добавляет проверку дублей ингредиентов

--- backend/api/validators.py
+++ backend/api/validators.py
@@ -2,8 +2,11 @@ from django.core.exceptions import ValidationError
 
 
 def ingredients_validator(ingredients):
+    ingredient_names = set()
+
     for ingredient in ingredients:
         amount = ingredient['amount']
+        name = ingredient['id']
 
         if int(amount) <= 0:
             raise ValidationError(
@@ -12,5 +15,9 @@ def ingredients_validator(ingredients):
                     ['Убедитесь, что это значение больше либо равно 1.']
                 }
             )
+        if name in ingredient_names:
+            raise ValidationError('Ингредиенты не должны повторяться.')
+
+        ingredient_names.add(name)
 
     return ingredients
