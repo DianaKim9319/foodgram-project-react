9f1475863a012abfcc76f33d0913d99688af205a
Diana Kim <Dianakim.1993@yandex.ru>
2023-11-14 01:09:13 +0900

    review fix 27

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,7 +6,6 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.response import Response
-from rest_framework.filters import SearchFilter
 from rest_framework.pagination import PageNumberPagination
 from djoser.views import UserViewSet
 from djoser.serializers import SetPasswordSerializer
