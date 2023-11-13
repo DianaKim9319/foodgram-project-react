7f1a65f0213d980dd2c0a09f8b6d62dbe1e4cb20
Diana Kim <Dianakim.1993@yandex.ru>
2023-11-13 23:31:40 +0900

    review fix 16

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -4,33 +4,29 @@ from recipes.models import Recipe, Tag
 
 
 class RecipeFilter(FilterSet):
-    """Кастомный фильтр для рецептов.
-    Для вкладок "Избранное, "Корзина" и тегов."""
     tags = filters.ModelMultipleChoiceFilter(
         field_name='tags__slug',
-        to_field_name='slug',
-        label='tags',
         queryset=Tag.objects.all(),
     )
-    is_favorited = filters.BooleanFilter(
+    is_favorite = filters.BooleanFilter(
         field_name='in_favorites__user',
         method='filter_by_favorites'
     )
-    is_in_shopping_cart = filters.BooleanFilter(
+    is_in_cart = filters.BooleanFilter(
         field_name='in_shopping_list__user',
         method='filter_by_shopping_cart'
     )
 
     class Meta:
         model = Recipe
-        fields = ('author', 'is_favorited', 'tags', 'is_in_shopping_cart', )
+        fields = ('author',  'tags', 'is_favorite', 'is_in_cart', )
 
-    def filter_by_favorites(self, queryset, name, value):
+    def filter_by_favorites(self, queryset, value):
         if value:
             return queryset.filter(in_favorites__user=self.request.user)
         return queryset.exclude(in_favorites__user=self.request.user)
 
-    def filter_by_shopping_cart(self, queryset, name, value):
+    def filter_by_shopping_cart(self, queryset, value):
         if value:
             return queryset.filter(in_shopping_list__user=self.request.user)
         return queryset.exclude(in_shopping_list__user=self.request.user)
