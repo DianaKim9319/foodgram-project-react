1cc7f123f605c535e7726b11499e1e27727c2151
Diana Kim <Dianakim.1993@yandex.ru>
2023-11-13 22:26:01 +0900

    review fix 11

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,31 +1,38 @@
-from django_filters import FilterSet, CharFilter, BooleanFilter
-from recipes.models import Recipe
+from django_filters.rest_framework import FilterSet, filters
+
+from recipes.models import Recipe, Tag
 
 
 class RecipeFilter(FilterSet):
-    author = CharFilter(method='filter_by_author')
-    tags = CharFilter(method='filter_by_tags')
-    is_in_shopping_cart = BooleanFilter(method='filter_by_shopping_cart')
-    is_favorited = BooleanFilter(method='filter_by_favorited')
+    """Кастомный фильтр для рецептов.
+    Для вкладок "Избранное, "Корзина" и тегов."""
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        label='tags',
+        queryset=Tag.objects.all(),
+    )
+    is_favorited = filters.BooleanFilter(
+        label='Favorited',
+        method='get_is_favorited',
+    )
+    is_in_shopping_cart = filters.BooleanFilter(
+        label='ShoppingCart',
+        method='get_is_in_shopping_cart',
+    )
 
     class Meta:
         model = Recipe
-        fields = ['author', 'tags', 'is_in_shopping_cart', 'is_favorited']
+        fields = ('author', 'is_favorited', 'tags', 'is_in_shopping_cart', )
 
-    def filter_by_author(self, queryset, name, value):
-        if value == 'me':
-            return queryset.filter(author=self.request.user)
-        return queryset.filter(author_id=value)
-
-    def filter_by_tags(self, queryset, name, value):
-        return queryset.filter(tags__slug__in=value)
-
-    def filter_by_shopping_cart(self, queryset, name, value):
+    def get_is_favorited(self, queryset, name, value):
         if value:
-            return queryset.filter(in_shopping_list__user=self.request.user)
-        return queryset.exclude(in_shopping_list__user=self.request.user)
+            return Recipe.objects.filter(selected__user=self.request.user)
+        return Recipe.objects.all()
 
-    def filter_by_favorited(self, queryset, name, value):
+    def get_is_in_shopping_cart(self, queryset, name, value):
         if value:
-            return queryset.filter(in_favorites__user=self.request.user)
-        return queryset.exclude(in_favorites__user=self.request.user)
+            return Recipe.objects.filter(
+                shopping_basket__user=self.request.user
+            )
+        return Recipe.objects.all()
