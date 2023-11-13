f684b005006fd2e85d149bccfcede469ebb0b85a
Diana Kim <Dianakim.1993@yandex.ru>
2023-11-14 00:17:20 +0900

    review fix 22

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.db.models import Sum, F
+from django.db.models import Sum
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, status
